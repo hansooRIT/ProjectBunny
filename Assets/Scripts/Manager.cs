e99c5eda685402d8779ddde1badb232bc8ace316
Steven Ma <sxm7686@rit.edu>
2018-01-26 00:15:32 -0500

    Added timer functionality.

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Manager : MonoBehaviour {
 
     public List<GameObject> bunnyList;
+    public float timeLeft = 3600f; //30 Minutes?
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,16 @@ public class Manager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        timeLeft -= Time.deltaTime;
+
+        if(timeLeft < 0)
+        {
+            //Insert Game Over function call
+        }
+
+        Debug.Log("Countdown: " + timeLeft);
+
 	}
 
     void SpawnBunny()
