fff7e87bbec440fe87447c51efce204e363021e2
HanSoo Lee <hxl9400@g.rit.edu>
2018-01-25 23:31:03 -0500

    Skeleton bunny and gamemanager classes, and got bunnies to spawn on button press

---
+++ Assets/Scripts/SpawnBunny.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SpawnBunny : MonoBehaviour {
+
+    public GameObject manager, bunny;
+    public Button spawnBunnyButton;
+    
+    // Use this for initialization
+    void Start () {
+        Button btn = spawnBunnyButton.GetComponent<Button>();
+        btn.onClick.AddListener(spawnBunny);
+    }
+
+    void spawnBunny()
+    {
+        GameObject newBunny = Instantiate(bunny, new Vector3(UnityEngine.Random.Range(-4.0f, -25.0f), UnityEngine.Random.Range(-3.0f, 3.0f), 0.0f), Quaternion.identity);
+        manager.GetComponent<Manager>().bunnyList.Add(newBunny);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
