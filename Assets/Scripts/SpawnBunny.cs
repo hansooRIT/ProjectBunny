1d9a77cdc98a50cfef9e7be79328ac85e6f56e8f
HanSoo Lee <hxl9400@g.rit.edu>
2018-01-26 01:12:07 -0500

    Minor bugfix to bunny spawning. Something didn't properly transfer in my last commit lmao.

--- Assets/Scripts/SpawnBunny.cs
+++ Assets/Scripts/SpawnBunny.cs
@@ -8,15 +8,12 @@ using UnityEngine.UI;
 public class SpawnBunny : MonoBehaviour {
 
     public GameObject manager, bunny;
-    public Button spawnBunnyButton;
     
     // Use this for initialization
     void Start () {
-        Button btn = spawnBunnyButton.GetComponent<Button>();
-        btn.onClick.AddListener(spawnBunny);
     }
 
-    void spawnBunny()
+    public void spawnBunny()
     {
         GameObject newBunny = Instantiate(bunny, new Vector3(UnityEngine.Random.Range(-4.0f, -25.0f), UnityEngine.Random.Range(-3.0f, 3.0f), 0.0f), Quaternion.identity);
         manager.GetComponent<Manager>().bunnyList.Add(newBunny);
