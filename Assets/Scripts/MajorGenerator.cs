e57f0e09f654e884841499379bbae09163f871cb
Aidan <ak3012@g.rit.edu>
2018-01-30 15:41:18 -0500

    Finished and integrated MajorGenerator class and UI Text

---
+++ Assets/Scripts/MajorGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MajorGenerator : MonoBehaviour
+{
+
+    private string[] MajorSubjectList = { "Dance", "Fitness", "Golf", "Health", "Outdoor", "Martial Arts", "Lifetime", "Varsity", "Religious", "Exercise", "Music", "Art", "Theatre" };
+    private string[] MajorActionList = { "Awareness", "Studies", "Science", "Theory", "History", "Education", "Training" };
+    private string Major;
+    public GameObject CanvasObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetNewMajor();
+    }
+
+    // Gets a new major
+    public void GetNewMajor()
+    {
+        Major = MajorSubjectList[Random.Range(0, MajorSubjectList.Length - 1)] + " " + MajorActionList[Random.Range(0, MajorActionList.Length - 1)];
+        CanvasObject.GetComponent<Text>().text += Major;
+    }
+
+    // Changes your current major
+    public void ChangeMajor()
+    {
+        Major = MajorSubjectList[Random.Range(0, MajorSubjectList.Length - 1)] + " " + MajorActionList[Random.Range(0, MajorActionList.Length - 1)];
+    }
+
+    // Gets current major
+    public string GetMajor()
+    {
+        return Major;
+    }
+}
