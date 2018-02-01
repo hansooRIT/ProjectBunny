53f1ffb393172531012206d11b59b6f9bf0a1ab9
HanSoo Lee <hxl9400@g.rit.edu>
2018-01-31 21:36:51 -0500

    Working scripts for non-canvas buttons.

---
+++ Assets/Scripts/BaseButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseButton : MonoBehaviour {
+
+    public bool clicked;
+    public float clickTimer;
+
+	// Use this for initialization
+	void Start () {
+        clicked = false;
+        clickTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        clickTimer += Time.deltaTime;
+        if (clickTimer > 0.3f)
+        {
+            clicked = false;
+        }
+	}
+
+    void OnMouseDown()
+    {
+        if (!clicked)
+        {
+            DoButtonAction();
+            clicked = true;
+            clickTimer = 0.0f;
+        }
+    }
+
+    public abstract void DoButtonAction();
+}
---
+++ Assets/Scripts/SpawnBunnyButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBunnyButton : BaseButton {
+
+    public GameObject manager, bunny;
+
+    public override void DoButtonAction()
+    {
+        GameObject newBunny = Instantiate(bunny, new Vector3(UnityEngine.Random.Range(-4.0f, -25.0f), UnityEngine.Random.Range(-3.0f, 3.0f), 0.0f), Quaternion.identity);
+        Debug.Log("Clicked UI Button");
+        //manager.GetComponent<Manager>().bunnyList.Add(newBunny);
+        //manager.GetComponent<DebtMeter>().spendMoney();
+        //Debug.Log("Current Debt: " + manager.GetComponent<DebtMeter>().getDebt());
+    }
+}
