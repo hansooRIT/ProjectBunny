859d7bd985381f5265cc8cbef445fa0220d6be62
Matthew Lefurge <msl5994@rit.edu>
2018-02-01 18:41:56 -0500

    Added dragging to the fence pieces and bunnies. Doesn't work on the button itself yet. Collisions don't work yet either.

---
+++ Assets/Scripts/FenceButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceButton : BaseButton {
+
+    public GameObject manager;
+    public GameObject penPieceImg;
+    /*
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    */
+    public override void DoButtonAction()
+    {
+        //Debug.Log("clicked fence button");
+        // instantiate a fence piece
+        GameObject temp = Instantiate(penPieceImg, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0.0f), Quaternion.identity);
+        // add to the list of fences in the manager
+        manager.GetComponent<Manager>().fenceList.Add(temp);
+        // add to the debt
+        manager.GetComponent<DebtMeter>().spendMoney();
+    }
+}
---
+++ Assets/Scripts/FenceCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "bunny")
+        {
+            Debug.Log("bunny hit the damn fence");
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "bunny")
+        {
+            Debug.Log("bunny hit fence");
+        }
+    }
+}
