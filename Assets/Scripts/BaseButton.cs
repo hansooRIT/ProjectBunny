3548259bde759e1d114315b24484a7ca51fdf1ce
Matt Lefurge <msl5994@g.rit.edu>
2018-02-12 11:07:33 -0500

    Rigidbodies are disabled while dragging

--- Assets/Scripts/BaseButton.cs
+++ Assets/Scripts/BaseButton.cs
@@ -19,6 +19,22 @@ public abstract class BaseButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+        // mouse button inputs
+        if(Input.GetMouseButtonDown(0))
+        {
+            // left click, perform buy action
+            // disable dragging
+            this.gameObject.GetComponent<DragObject>().enabled = false;
+            Debug.Log("left click");
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            // enable dragging
+            this.gameObject.GetComponent<DragObject>().enabled = true;
+            Debug.Log("right click");
+        }*/
+
         clickTimer += Time.deltaTime;
         if (clickTimer > 0.3f)
         {
--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 public class DragObject : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+
 	}
+
+    // disable the collider on click
+    private void OnMouseDown()
+    {
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
 
     private void OnMouseDrag()
     {
+        Debug.Log("OnMOuseDrag");
         float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
         transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
+
+        // disable the rigidbody while dragging
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    private void OnMouseUp()
+    {
+        // re-enable the collider
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
     }
 }
