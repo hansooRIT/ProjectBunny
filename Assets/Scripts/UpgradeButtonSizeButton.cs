b9702191ac20236fdc376caf20b62943ee89f592
Matthew Lefurge <msl5994@rit.edu>
2018-02-14 19:27:19 -0500

    Created a button that upgrades the size of buttons. Tweaked some values for gameobject masses.

---
+++ Assets/Scripts/UpgradeButtonSizeButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeButtonSizeButton : BaseButton {
+
+    // list of buttons
+    public GameObject[] buttons;
+    // separate check for the sellButton
+    private GameObject sellButton;
+
+    /*
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    */
+    public override void DoButtonAction()
+    {
+        // get the buttons
+        buttons = GameObject.FindGameObjectsWithTag("Button");
+        sellButton = GameObject.FindGameObjectWithTag("SellButton");
+        
+        // loop through buttons
+        foreach(GameObject button in buttons)
+        {
+            // set the new scale
+            button.transform.localScale += new Vector3(0.2f, 0.2f, 0.0f);
+        }
+
+        // set Sell Button scale
+        sellButton.transform.localScale += new Vector3(0.2f, 0.2f, 0.0f);
+
+        clearClick(); // resets the click variable
+    }
+}
